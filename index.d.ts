922973812108d56969dbf6062f3f33ab07bcce91
Damian Kaczmarek <rush@rushbase.net>
2026-09-05 14:38:44 -0500

    Published by publish-to-git 88aedbc14c220e2fbd02c768b96bbf84fe864611 (HEAD -> refs/heads/master, refs/remotes/origin/master, refs/remotes/origin/HEAD) v9.0.3

---
+++ index.d.ts
@@ -0,0 +1,34 @@
+declare namespace createContext {
+  interface STACKGL_destroy_context {
+      destroy(): void;
+  }
+
+  interface STACKGL_resize_drawingbuffer {
+      resize(width: GLint, height: GLint): void;
+  }
+
+  interface StackGLExtension {
+      getExtension(extensionName: "STACKGL_destroy_context"): STACKGL_destroy_context | null;
+      getExtension(extensionName: "STACKGL_resize_drawingbuffer"): STACKGL_resize_drawingbuffer | null;
+  }
+}
+
+declare function createContext(
+  width: number,
+  height: number,
+  options?: WebGLContextAttributes & { createWebGL2Context?: false; useSwiftShader?: boolean; useVulkan?: boolean },
+): WebGLRenderingContext & createContext.StackGLExtension;
+
+declare function createContext(
+  width: number,
+  height: number,
+  options: WebGLContextAttributes & { createWebGL2Context: true; useSwiftShader?: boolean; useVulkan?: boolean }
+): WebGL2RenderingContext & createContext.StackGLExtension;
+
+declare function createContext(
+  width: number,
+  height: number,
+  options?: WebGLContextAttributes & { createWebGL2Context?: boolean; useSwiftShader?: boolean; useVulkan?: boolean }
+): (WebGLRenderingContext | WebGL2RenderingContext) & createContext.StackGLExtension;
+
+export = createContext;
